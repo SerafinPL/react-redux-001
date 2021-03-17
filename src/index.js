b6e28a160b235f65858d4ea0c1a0e2faa0c67bf4
serafinpl <ser.burberibar@gmail.com>
2021-03-17 15:02:51 +0100

    addong a middleware

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import {createStore, combineReducers} from 'redux';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
 import counterReducer from './store/reducers/counter';
 import resultsReducer from './store/reducers/results';
 import { Provider } from 'react-redux'
@@ -12,9 +12,20 @@ import { Provider } from 'react-redux'
 const rootReducer = combineReducers({
 	ctr: counterReducer,
 	res: resultsReducer
-})
+});
+//middleware
+const logger = store => {
+	return next =>{
+		return action => {
+			console.log('[Middleware] Dispatching', action);
+			const result = next(action);
+			console.log('[Middleware] next state', store.getState());
+			return result;
+		};
+	};
+};
 
-const storeBox = createStore(rootReducer/*reducer*/);
+const storeBox = createStore(rootReducer/*reducer*/, applyMiddleware(logger));
 
 ReactDOM.render(
 	<Provider store={storeBox}>
